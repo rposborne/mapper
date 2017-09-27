02cf3055a6f645911e87686b1a50b9fc41598a7b
Emily Deans <emilyldeans@gmail.com>
2017-09-27 17:09:16 -0400

    Created initial maps controller methods.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,38 @@
 class MapsController < ApplicationController
   def index
+    @maps = Map.all
   end
+
+  def new
+    @map = Map.new
+  end
+
+  def show
+    @map = Map.find(params[:id])
+  end
+
+  def create
+    @map = Map.new(map_params)
+      if @map.save
+        redirect_to @map
+      else
+        render 'new'
+      end
+  end
+
+  def edit
+    @map = Map.find(params[:id])
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def map_params
+      params.require(:map).permit(:title, :description)
+      # will need marker permits here as well
+    end
+
 end
