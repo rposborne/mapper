dfe7c68e3500c99b307a059ab226cf2c01833167
Emily Deans <emilyldeans@gmail.com>
2017-09-27 16:46:31 -0400

    Adjusted route config and established root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+root to: 'welcomes#index'
+
+resources :users
+resources :maps do
+  resources :markers
+end
+resource :session, only: [:new, :create, :destroy]
+
+
   get 'maps/index'
-
   get 'sessions/index'
-
   get 'sessions/new'
-
-  get 'welcomes/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
