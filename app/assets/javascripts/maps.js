97d7091944a2b1b220e5388e7b7b47291d4807a4
Emily Deans <emilyldeans@gmail.com>
2017-09-27 17:58:02 -0400

    Enabled map in new map view using the GoogleMaps API.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
