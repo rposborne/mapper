82f65cab6dfb8f0b03e82c19dbb34c5ab1751a67
Emily Deans <emilyldeans@gmail.com>
2017-09-27 16:43:12 -0400

    Created controllers for maps, welcomes, users, and sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'maps/index'
+
+  get 'sessions/index'
+
+  get 'sessions/new'
+
+  get 'welcomes/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
