42d823e46b0ba939900cca0b496016bbb7dcab1b
Emily Deans <emilyldeans@gmail.com>
2017-09-28 10:32:33 -0400

    Added map_id field to marker model and created markers controller.

---
+++ app/controllers/markers_controller.rb
@@ -0,0 +1,20 @@
+class MarkersController < ApplicationController
+  def index
+    @markers = Marker.all
+  end
+
+  def new
+    @marker = Marker.new
+  end
+
+  def create
+    @map = Map.find(params[:map_id])
+    @marker = @map.marker.build(marker_params)
+  end
+
+  private
+
+    def marker_params
+      params.require(:marker).permit(:address, :lat, :lng)
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927203352) do
+ActiveRecord::Schema.define(version: 20170928143132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170927203352) do
     t.decimal "lng"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "map_id"
   end
 
   create_table "users", force: :cascade do |t|
