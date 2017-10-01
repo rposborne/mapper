39d67d82de75cd953573f09a46a720969de46859
Emily Deans <emilyldeans@gmail.com>
2017-10-01 09:12:06 -0400

    Generated views for user using devise.

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,14 @@ ActiveRecord::Schema.define(version: 20170929152841) do
     t.json "lat_lng"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
