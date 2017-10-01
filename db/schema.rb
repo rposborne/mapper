00cdf5a19de42c725fa7457ee1d49438d4de7daa
Emily Deans <emilyldeans@gmail.com>
2017-10-01 10:29:08 -0400

    Created email field for user model to generate successful sign-up/login views.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001132432) do
+ActiveRecord::Schema.define(version: 20171001142745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171001132432) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
