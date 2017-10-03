5a0b9057db3299ddafcbc34fc7acdd0d4232d07e
Emily Deans <emilyldeans@gmail.com>
2017-10-03 13:19:14 -0400

    Created actions in the users and sessions controllers.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
-  def index
+  def create
+    user = User.find_by_email(params[:email])
+
+    if @user && user.authenticate(params[:password_digest])
+      session[:user_id] = user.id
+      redirect_to users_path
+    else
+      redirect_to new_session_path
+    end
   end
 
-  def new
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,43 @@
 class UsersController < ApplicationController
 
-  def show
+  def index
+      @user = User.all
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path @user
+        else
+          render 'new'
+        end
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def destroy
+      @user = User.destroy(params[:id])
+      respond_to do |format|
+        format.html { redirect_to user_path }
+      end
+    end
+
+    private
+
+      def user_params
+        params.require(:user).permit(:id, :email, :encrypted_password)
+    end
 
   end
-
-  def create
-
-  end
-
-end
