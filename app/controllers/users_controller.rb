aa697db6e09e7467769a9450deb2bf0c7726623f
Emily Deans <emilyldeans@gmail.com>
2017-10-03 14:15:52 -0400

    Restructured controllers and views to work with default devise authentication.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,35 +4,6 @@ class UsersController < ApplicationController
       @user = User.all
     end
 
-    def new
-      @user = User.new
-    end
-
-    def show
-      @user = User.find(params[:id])
-    end
-
-    def create
-      @user = User.new(user_params)
-
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to user_path @user
-        else
-          render 'new'
-        end
-    end
-
-    def edit
-      @user = User.find(params[:id])
-    end
-
-    def destroy
-      @user = User.destroy(params[:id])
-      respond_to do |format|
-        format.html { redirect_to user_path }
-      end
-    end
 
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,6 @@ resources :users
 resources :maps do
   resources :markers
 end
-resource :session, only: [:new, :create, :destroy]
-
-
-  get 'maps/index'
-  get 'sessions/index'
-  get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
