d45302a15cd2dc35b0060aebf910fa69270cb40c
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-10-23 17:23:11 -0400

    Wrapped in an IIFE

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
 (function() {
   window.MapSpot = function(node, inputNode, options) {
     const DEFAULT_CENTER = { lat: 38.898754, lng: -77.031563 };
@@ -292,51 +291,50 @@
       };
     };
   };
-})();
 
-(function() {
   window.mapDefaults = {
     markers: [{ lat: 38.898754, lng: -77.031563 }],
     node: document.getElementById("map"),
     inputNode: document.getElementById("pac-input")
   };
+
+  if (document.getElementById('map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+  }
+
+  if (document.getElementById('save-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
+  if (document.getElementById('save-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
+  if (document.getElementById('show-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
+  if (document.getElementById('edit-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
 })();
-
-if (document.getElementById('map-page') !== null) {
-  window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
-    center: mapDefaults.markers[0]
-  });
-  mapSpot.initMap();
-}
-
-if (document.getElementById('save-map-page') !== null) {
-  window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
-    center: mapDefaults.markers[0]
-  });
-  mapSpot.initMap();
-  mapSpot.getMap();
-}
-
-if (document.getElementById('save-map-page') !== null) {
-  window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
-    center: mapDefaults.markers[0]
-  });
-  mapSpot.initMap();
-  mapSpot.getMap();
-}
-
-if (document.getElementById('show-map-page') !== null) {
-  window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
-    center: mapDefaults.markers[0]
-  });
-  mapSpot.initMap();
-  mapSpot.getMap();
-}
-
-if (document.getElementById('edit-map-page') !== null) {
-  window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
-    center: mapDefaults.markers[0]
-  });
-  mapSpot.initMap();
-  mapSpot.getMap();
-}
