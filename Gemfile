df9bd3a109166deaac5b77ae28b2a9a13735af2a
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-10-23 18:10:10 -0400

    Installed webpacker

--- Gemfile
+++ Gemfile
@@ -16,7 +16,6 @@ gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
-gem 'sprockets-es6'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
@@ -33,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'devise'
+gem 'webpacker', '~> 3.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/client/maps.js
@@ -0,0 +1,55 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+import MapSpot from './map_spot'
+
+(function() {
+  window.mapDefaults = {
+    markers: [{ lat: 38.898754, lng: -77.031563 }],
+    node: document.getElementById("map"),
+    inputNode: document.getElementById("pac-input")
+  };
+
+  if (document.getElementById('map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+  }
+
+  if (document.getElementById('save-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
+  if (document.getElementById('save-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
+  if (document.getElementById('show-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
+  if (document.getElementById('edit-map-page') !== null) {
+    window.mapSpot = new MapSpot(mapDefaults.node, mapDefaults.inputNode, {
+      center: mapDefaults.markers[0]
+    });
+    mapSpot.initMap();
+    mapSpot.getMap();
+  }
+
+
+})();
+
+export default MapSpot;
